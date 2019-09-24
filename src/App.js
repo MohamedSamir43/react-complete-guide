e63c440ed799d3e3ac4edae813c834fabb93e7d1
Mohamed Samir <mohamed2.samir.ext@orange.com>
2019-09-24 22:34:53 +0200

    video 9 of module 4

--- src/App.js
+++ src/App.js
@@ -24,24 +24,19 @@ class App extends Component {
 		showPersons: false,
 	};
 
-	nameChangedHandler = event => {
-		// console.log("Was clicked!");
-		// this.state.persons[0].name = 'Maximilian' Dont change the state directly like this
+	nameChangedHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex(p => {
+			return p.id === id;
+		});
+		const person = { ...this.state.persons[personIndex] };
+
+		person.name = event.target.value;
+
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
+
 		this.setState({
-			persons: [
-				{
-					name: "Maximilian",
-					age: 28,
-				},
-				{
-					name: event.target.value,
-					age: 29,
-				},
-				{
-					name: "Stephanie",
-					age: 27,
-				},
-			],
+			persons: persons,
 		});
 	};
 
@@ -67,7 +62,7 @@ class App extends Component {
 			persons = (
 				<div>
 					{this.state.persons.map((person, index) => {
-						return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} key={person.id} />;
+						return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} key={person.id} changed={event => this.nameChangedHandler(event, person.id)} />;
 					})}
 				</div>
 			);
