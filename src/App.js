467ec1dd88069602d7c8261791938e014a6fe03e
Mohamed Samir <mohamed2.samir.ext@orange.com>
2019-09-25 00:44:26 +0200

    video 6 of module 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import Radium from 'radium'
+import Radium, { StyleRoot } from 'radium'
 import Person from './Person/Person'
 class App extends Component {
     state = {
@@ -92,14 +92,16 @@ class App extends Component {
 
         if (this.state.persons.length <= 1) classes.push('bold')
         return (
-            <div className="App">
-                <h1>Hi, i'm a react App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button style={style} onClick={this.togglePersonsHandler}>
-                    Toggle Persons
-                </button>
-                {persons}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi, i'm a react App</h1>
+                    <p className={classes.join(' ')}>This is really working!</p>
+                    <button style={style} onClick={this.togglePersonsHandler}>
+                        Toggle Persons
+                    </button>
+                    {persons}
+                </div>
+            </StyleRoot>
         )
         // return React.createElement("div", { className: "App" }, React.createElement("h1", null, "Hi react app"));
     }
