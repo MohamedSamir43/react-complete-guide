675ce13021f80df5318e37c71c8e76cb95e831a4
Mohamed Samir <mohamed2.samir.ext@orange.com>
2019-10-02 19:53:09 +0200

    video 5 module 7

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -13,7 +13,7 @@ const cockpit = props => {
 
     return (
         <div className={classes.CockPit}>
-            <h1>Hi, i'm a react App</h1>
+            <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button className={btnClass} onClick={props.clicked}>
                 Toggle Persons
