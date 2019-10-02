ea78ecb7125eac2191c39f3dfa696bb21650b9cb
Mohamed Samir <mohamed2.samir.ext@orange.com>
2019-10-02 22:43:29 +0200

    video 9 of module 7

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,10 @@ import classes from './App.css'
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 class App extends Component {
+    constructor(props) {
+        super(props)
+        console.log('[App.js] constructor')
+    }
     state = {
         persons: [
             {
@@ -26,6 +30,20 @@ class App extends Component {
         showPersons: false,
     }
 
+    static getDerivedStateFromProps(props, state) {
+        console.log('[App.js] getDerivedStateFromProps', props)
+        return state
+    }
+    componentDidMount() {
+        console.log('[App.js] componentDidMount')
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[App.js] shouldComponentUpdate')
+        return true
+    }
+    componentDidUpdate() {
+        console.log('[App.js] componentDidUpdate')
+    }
     nameChangedHandler = (event, id) => {
         const personIndex = this.state.persons.findIndex(p => {
             return p.id === id
@@ -52,6 +70,7 @@ class App extends Component {
         this.setState({ showPersons: !doesShow })
     }
     render() {
+        console.log('[App.js] render')
         let persons = null
 
         if (this.state.showPersons) {
